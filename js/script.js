b7436c986c467571d6ad9442a7b7f991237823fb
NicPWNs <nic@nicpjones.com>
2023-08-27 21:56:47 -0400

    feat: add referrer block

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
 function vote() {
   var nav = document.getElementById("nav");
   var vote = document.getElementById("vote");
-  nav.style.display = "none";
-  vote.style.display = "block";
+  var error = document.getElementById("error");
+  var discord = document.getElementById("discord");
+  var sub;
+  var ref = document.referrer;
+  if (ref.match(/^https?:\/\/([^\/]+\.)?adfoc\.us(\/|$)/i)) {
+    nav.style.display = "none";
+    vote.style.display = "block";
+  } else {
+    vote.style.display = "block";
+    discord.style.display = "none";
+    submit.style.display = "none";
+    error.style.display = "block";
+    error.value = "Use /vote in the MEGACORD";
+  }
 }
 function sendVote() {
   var discord = document.getElementById("discord");
   var error = document.getElementById("error");
+  var submit = document.getElementById("submit");
   if (discord.value != "") {
-    document.getElementById("submit").style.display = "none";
+    submit.style.display = "none";
     error.style.display = "none";
     discord.style.background = "#57f287";
     discord.style.fontWeight = "bold";

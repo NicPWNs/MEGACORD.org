77e01ad1712aa46a7b4b52d125e918376ce1604b
NicPWNs <nic@nicpjones.com>
2023-08-30 21:27:10 -0400

    feat: finish voting functionality

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
+const date = new Date();
+let day = date.getDate().toString().padStart(2, "0");
+let month = (date.getMonth() + 1).toString().padStart(2, "0");
+let year = date.getFullYear();
+
 function vote() {
   var nav = document.getElementById("nav");
   var vote = document.getElementById("vote");
   var error = document.getElementById("error");
   var discord = document.getElementById("discord");
-  var sub;
+  discord.value = "";
   var ref = document.referrer;
-  if (ref.match(/^https?:\/\/([^\/]+\.)?adfoc\.us(\/|$)/i)) {
+
+  if (ref.match("/^https?://([^/]+.)?adfoc.us(/|$)/i")) {
     nav.style.display = "none";
     vote.style.display = "block";
   } else {
@@ -16,20 +22,64 @@ function vote() {
     error.value = "Use /vote in the MEGACORD";
   }
 }
-function sendVote() {
+
+const sendVote = async () => {
   var discord = document.getElementById("discord");
   var error = document.getElementById("error");
   var submit = document.getElementById("submit");
-  if (discord.value != "") {
-    submit.style.display = "none";
-    error.style.display = "none";
-    discord.style.background = "#57f287";
-    discord.style.fontWeight = "bold";
-    discord.style.borderRadius = "5px";
-    discord.value = "Vote Successful!";
-    discord.readOnly = true;
+
+  await fetch(".env")
+    .then((res) => res.text())
+    .then((text) => {
+      key = text.split("=")[1];
+    })
+    .catch((e) => console.error(e));
+
+  if (discord.value) {
+    const response = await fetch("https://api.megacord.org/items", {
+      method: "GET",
+      headers: {
+        authorization: key,
+      },
+    });
+    const json = await response.json();
+    const user = json.filter((user) => user.username == discord.value);
+
+    if (json.filter((user) => user.username == discord.value).length >= 1) {
+      submit.style.display = "none";
+      error.style.display = "none";
+
+      data = {
+        id: user[0].id,
+        date: user[0].date,
+        username: user[0].username,
+        voted: true,
+      };
+
+      await fetch("https://api.megacord.org/items", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: key,
+        },
+        body: JSON.stringify(data),
+      });
+
+      discord.style.background = "#57f287";
+      discord.style.fontWeight = "bold";
+      discord.style.borderRadius = "5px";
+      discord.value = "Vote Successful!";
+      discord.readOnly = true;
+    } else {
+      error.style.display = "block";
+      error.value = "Invalid Discord Username";
+      console.error(
+        "Invalid Discord Username was provided. Not found in database."
+      );
+    }
   } else {
     error.style.display = "block";
     error.value = "Enter a Discord Username";
+    console.error("No Discord Username was provided.");
   }
-}
+};
